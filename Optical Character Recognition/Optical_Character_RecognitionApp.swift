730a9d5cba3dbd9b22a0526ff99f26c12cb56777
Đỗ Minh Quân <minhquan.5322@aol.com>
2022-12-04 16:46:44 +0700

    Initial Commit

---
+++ Optical Character Recognition/Optical_Character_RecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Optical_Character_RecognitionApp.swift
+//  Optical Character Recognition
+//
+//  Created by Minh Quan on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Optical_Character_RecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
